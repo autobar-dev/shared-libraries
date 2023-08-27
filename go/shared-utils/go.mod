cd9f672646ef96449f4a5260455f38d7177bcce0
Adam Pisula <adam.pisula@outlook.com>
2023-08-27 19:44:15 +0200

    make post request shared util

---
+++ go/shared-utils/go.mod
@@ -0,0 +1,3 @@
+module github.com/autobar-dev/shared-libraries/go/shared-utils
+
+go 1.21.0
---
+++ go/shared-utils/main.go
@@ -0,0 +1,30 @@
+package sharedutils
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+)
+
+func NewPostRequest(
+	sender_microservice_name string,
+	url string,
+	body interface{},
+) (*http.Request, error) {
+	body_json, err := json.Marshal(body)
+	if err != nil {
+		return nil, err
+	}
+
+	body_reader := bytes.NewReader(body_json)
+
+	req, err := http.NewRequest(http.MethodPost, url, body_reader)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Add("X-Internal", sender_microservice_name)
+	req.Header.Add("Content-Type", "application/json")
+
+	return req, err
+}
