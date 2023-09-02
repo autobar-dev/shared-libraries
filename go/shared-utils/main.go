4cf00e361d0eef50562334c767e72b3f258983e1
Adam Pisula <adam.pisula@outlook.com>
2023-09-02 18:16:14 +0200

    added get request to shared utils

--- go/shared-utils/main.go
+++ go/shared-utils/main.go
@@ -34,3 +34,24 @@ func NewPostRequest(
 
 	return res, nil
 }
+
+func NewGetRequest(
+	client *http.Client,
+	sender_microservice_name string,
+	url string,
+) (*http.Response, error) {
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Add("X-Internal", sender_microservice_name)
+	req.Header.Add("Content-Type", "application/json")
+
+	res, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	return res, nil
+}
