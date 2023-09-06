40c644c84b390f8494fdf0e99ad5c78e5c546d51
Adam Pisula <adam.pisula@outlook.com>
2023-09-06 22:51:18 +0200

    added generate random string to shared utils

---
+++ go/shared-utils/http.go
@@ -0,0 +1,57 @@
+package sharedutils
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+)
+
+func NewPostRequest(
+	client *http.Client,
+	sender_microservice_name string,
+	url string,
+	body interface{},
+) (*http.Response, error) {
+	body_json, err := json.Marshal(body)
+	if err != nil {
+		return nil, err
+	}
+
+	body_reader := bytes.NewReader(body_json)
+
+	req, err := http.NewRequest(http.MethodPost, url, body_reader)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Add("X-Internal", sender_microservice_name)
+	req.Header.Add("Content-Type", "application/json")
+
+	res, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	return res, nil
+}
+
+func NewGetRequest(
+	client *http.Client,
+	sender_microservice_name string,
+	url string,
+) (*http.Response, error) {
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Add("X-Internal", sender_microservice_name)
+	req.Header.Add("Content-Type", "application/json")
+
+	res, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	return res, nil
+}
---
+++ go/shared-utils/random.go
@@ -0,0 +1,20 @@
+package sharedutils
+
+import "math/rand"
+
+var (
+	LowercaseCharacterSet                = []rune("abcdefghijklmnopqrstuvwxyz")
+	UppercaseCharacterSet                = []rune("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+	NumberCharacterSet                   = []rune("0123456789")
+	LowercaseUppercaseCharacterSet       = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+	LowercaseUppercaseNumberCharacterSet = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+)
+
+func GenerateRandomString(length int, character_set []rune) string {
+	b := make([]rune, length)
+	for i := range b {
+		b[i] = character_set[rand.Intn(len(character_set))]
+	}
+
+	return string(b)
+}
