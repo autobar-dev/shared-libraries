c3f64c39a595562c447d50957d5076f0b4483e2c
Adam Pisula <adam.pisula@outlook.com>
2023-09-24 19:29:46 +0200

    module repository with module type

---
+++ go/module-repository/module_repository.go
@@ -0,0 +1,15 @@
+package productrepository
+
+import (
+	"net/http"
+)
+
+func NewModuleRepository(service_url string, microservice_name string) *ModuleRepository {
+	client := &http.Client{}
+
+	return &ModuleRepository{
+		service_url:       service_url,
+		http_client:       client,
+		microservice_name: microservice_name,
+	}
+}
---
+++ go/module-repository/types.go
@@ -0,0 +1,23 @@
+package productrepository
+
+import (
+	"net/http"
+	"time"
+)
+
+type ModuleRepository struct {
+	service_url       string
+	http_client       *http.Client
+	microservice_name string
+}
+
+type Module struct {
+	Id           int32          `json:"id"`
+	SerialNumber string         `json:"serial_number"`
+	StationId    *string        `json:"station_id"`
+	ProductId    *string        `json:"product_id"`
+	Enabled      bool           `json:"enabled"`
+	Prices       map[string]int `json:"prices"`
+	CreatedAt    time.Time      `json:"created_at"`
+	UpdatedAt    time.Time      `json:"updated_at"`
+}
