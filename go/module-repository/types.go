0be7621abfb6a46a9254cb1f9678d31a6163ad09
Adam Pisula <adam.pisula@outlook.com>
2023-10-12 00:33:49 +0200

    added amount to display unit

--- go/module-repository/types.go
+++ go/module-repository/types.go
@@ -26,6 +26,7 @@ type Module struct {
 
 type DisplayUnit struct {
 	Id                     int32     `json:"id"`
+	Amount                 float64   `json:"amount"`
 	Symbol                 string    `json:"symbol"`
 	DivisorFromMillilitres float64   `json:"divisor_from_millilitres"`
 	DecimalsDisplayed      int32     `json:"decimals_displayed"`
