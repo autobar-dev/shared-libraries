bff88904aa47af34bf4983377cee3069932e15b9
Adam Pisula <adam.pisula@outlook.com>
2023-10-12 00:08:14 +0200

    added display currency

--- go/module-repository/types.go
+++ go/module-repository/types.go
@@ -12,15 +12,16 @@ type ModuleRepository struct {
 }
 
 type Module struct {
-	Id           int32          `json:"id"`
-	SerialNumber string         `json:"serial_number"`
-	StationId    *string        `json:"station_id"`
-	ProductId    *string        `json:"product_id"`
-	Enabled      bool           `json:"enabled"`
-	Prices       map[string]int `json:"prices"`
-	DisplayUnit  DisplayUnit    `json:"display_unit"`
-	CreatedAt    time.Time      `json:"created_at"`
-	UpdatedAt    time.Time      `json:"updated_at"`
+	Id              int32          `json:"id"`
+	SerialNumber    string         `json:"serial_number"`
+	StationId       *string        `json:"station_id"`
+	ProductId       *string        `json:"product_id"`
+	Enabled         bool           `json:"enabled"`
+	Prices          map[string]int `json:"prices"`
+	DisplayCurrency string         `json:"display_currency"`
+	DisplayUnit     DisplayUnit    `json:"display_unit"`
+	CreatedAt       time.Time      `json:"created_at"`
+	UpdatedAt       time.Time      `json:"updated_at"`
 }
 
 type DisplayUnit struct {
