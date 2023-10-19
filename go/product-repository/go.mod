db0c8f0586ebef931e352abde3dca7e148dd832f
Adam Pisula <adam.pisula@outlook.com>
2023-10-19 13:04:18 +0200

    using filerepository.File for cover image

--- go/product-repository/go.mod
+++ go/product-repository/go.mod
@@ -2,4 +2,6 @@ module github.com/autobar-dev/shared-libraries/go/product-repository
 
 go 1.21.0
 
-require github.com/autobar-dev/shared-libraries/go/shared-utils v0.0.0-20230906220457-9b9db8991f98
+require github.com/autobar-dev/shared-libraries/go/shared-utils v0.0.0-20231019105420-bb6feaaeef8e
+
+require github.com/autobar-dev/shared-libraries/go/file-repository v0.0.0-20231019105717-58ea9ed5f03d // indirect
--- go/product-repository/types.go
+++ go/product-repository/types.go
@@ -3,6 +3,8 @@ package productrepository
 import (
 	"net/http"
 	"time"
+
+	filerepository "github.com/autobar-dev/shared-libraries/go/file-repository"
 )
 
 type ProductRepository struct {
@@ -12,13 +14,13 @@ type ProductRepository struct {
 }
 
 type Product struct {
-	Id           string            `json:"id"`
-	Names        map[string]string `json:"names"`
-	Descriptions map[string]string `json:"descriptions"`
-	Cover        string            `json:"cover"`
-	Enabled      bool              `json:"enabled"`
-	CreatedAt    time.Time         `json:"created_at"`
-	UpdatedAt    time.Time         `json:"updated_at"`
+	Id           string              `json:"id"`
+	Names        map[string]string   `json:"names"`
+	Descriptions map[string]string   `json:"descriptions"`
+	Cover        filerepository.File `json:"cover"`
+	Enabled      bool                `json:"enabled"`
+	CreatedAt    time.Time           `json:"created_at"`
+	UpdatedAt    time.Time           `json:"updated_at"`
 }
 
 type ProductResponseType string
