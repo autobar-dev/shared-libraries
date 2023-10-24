4353140f6c3a48a6091b5593fa0aed19a47d2c82
Adam Pisula <adam.pisula@outlook.com>
2023-10-25 00:21:18 +0200

    product badges

--- go/product-repository/types.go
+++ go/product-repository/types.go
@@ -13,12 +13,26 @@ type ProductRepository struct {
 	microservice_name string
 }
 
+type ProductBadgeType string
+
+const (
+	ProductBadgeTypePrimary   ProductBadgeType = "primary"
+	ProductBadgeTypeSecondary ProductBadgeType = "secondary"
+)
+
+type ProductBadge struct {
+	Type  ProductBadgeType `json:"type"`
+	Label string           `json:"label"`
+	Value *string          `json:"value"`
+}
+
 type Product struct {
 	Id           string              `json:"id"`
 	Names        map[string]string   `json:"names"`
 	Descriptions map[string]string   `json:"descriptions"`
 	Cover        filerepository.File `json:"cover"`
 	Enabled      bool                `json:"enabled"`
+	Badges       []ProductBadge      `json:"badges"`
 	CreatedAt    time.Time           `json:"created_at"`
 	UpdatedAt    time.Time           `json:"updated_at"`
 }
