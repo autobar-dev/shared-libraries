cb00ec7b06fd9b2f4367405d10793a0ca4fcc8fe
Adam Pisula <adam.pisula@outlook.com>
2026-03-05 18:03:15 +0100

    added file from filerepo to product

--- go/product-repository/go.mod
+++ go/product-repository/go.mod
@@ -4,4 +4,4 @@ go 1.21.0
 
 require github.com/autobar-dev/shared-libraries/go/shared-utils v0.0.0-20231019105420-bb6feaaeef8e
 
-require github.com/autobar-dev/shared-libraries/go/file-repository v0.0.0-20231019105717-58ea9ed5f03d // indirect
+require github.com/autobar-dev/shared-libraries/go/file-repository v0.0.0-20260305161326-b0c12750bd2c // indirect
--- go/product-repository/types.go
+++ go/product-repository/types.go
@@ -3,6 +3,8 @@ package productrepository
 import (
 	"net/http"
 	"time"
+
+	filerepository "github.com/autobar-dev/shared-libraries/go/file-repository"
 )
 
 type ProductRepository struct {
@@ -25,15 +27,15 @@ type ProductBadge struct {
 }
 
 type Product struct {
-	Id           int               `json:"id"`
-	Slug         string            `json:"slug"`
-	ImageFileId  int               `json:"image_file_id"`
-	Names        map[string]string `json:"names"`
-	Descriptions map[string]string `json:"descriptions"`
-	Enabled      bool              `json:"enabled"`
-	Badges       []ProductBadge    `json:"badges"`
-	CreatedAt    time.Time         `json:"created_at"`
-	UpdatedAt    time.Time         `json:"updated_at"`
+	Id           int                 `json:"id"`
+	Slug         string              `json:"slug"`
+	Cover        filerepository.File `json:"cover"`
+	Names        map[string]string   `json:"names"`
+	Descriptions map[string]string   `json:"descriptions"`
+	Enabled      bool                `json:"enabled"`
+	Badges       []ProductBadge      `json:"badges"`
+	CreatedAt    time.Time           `json:"created_at"`
+	UpdatedAt    time.Time           `json:"updated_at"`
 }
 
 type ProductResponseType string
